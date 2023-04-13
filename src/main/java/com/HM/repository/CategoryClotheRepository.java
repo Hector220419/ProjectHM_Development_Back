9e6ac413269515f48d22757a9a7baa0caac307e0
Hector010123 <hg3438773@gmail.com>
2023-04-13 16:38:16 -0600

    Cambio de consulta repository para tabla de categoryClothe

--- src/main/java/com/HM/repository/CategoryClotheRepository.java
+++ src/main/java/com/HM/repository/CategoryClotheRepository.java
@@ -8,7 +8,7 @@ import com.HM.model.CategoryClothe;
 
 public interface CategoryClotheRepository extends JpaRepository<CategoryClothe, Integer>{
 	
-	@Query("SELECT p FROM CategoryClothe p WHERE name = ?1") // Se utiliza el primer parametro encontrado
+	@Query("SELECT p FROM CategoryClothe p WHERE nameCategory = ?1") // Se utiliza el primer parametro encontrado
 	
 	// Metodo que realiza la consulta personalizada definida anteriormente. Retorna un optional que puede contener un objeto Product si se encuentra en la base de datos (tiene que ser con el nombre especificado), o un optional vacio si no existe.
 	
